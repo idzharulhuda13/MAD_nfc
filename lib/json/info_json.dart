416b70be7400a81351e2c89247566d3dbfb344cb
idzharulhuda13 <idzharul.huda@gmail.com>
2021-06-18 08:58:41 +0700

    Add Info Data using JSON

---
+++ lib/json/info_json.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+List itemsType = [
+  {"icon": Icons.circle, "type": "TOL"},
+  {"icon": Icons.circle, "type": "TransJakarta"},
+  {"icon": Icons.circle, "type": "MRT"},
+  {"icon": Icons.circle, "type": "LRT"}
+];
+
+List infoTol = [
+  {"rute": "RUTE", "tarif": "TARIF"},
+  {"rute": "Merak - Jakarta", "tarif": "67500"},
+  {"rute": "Tangerang - Jakarta", "tarif": "23500"},
+  {"rute": "JORR - Cawang", "tarif": "16500"},
+  {"rute": "Cikampek - Jakarta", "tarif": "20000"},
+  {"rute": "Subang - Jakarta", "tarif": "55000"},
+  {"rute": "Indramayu - Jakarta", "tarif": "81500"},
+  {"rute": "Kertajati - Jakarta", "tarif": "99000"},
+  {"rute": "Palimanan - Jakarta", "tarif": "127500"},
+  {"rute": "Cirebon - Jakarta", "tarif": "134000"},
+  {"rute": "Kanci - Jakarta", "tarif": "140000"},
+  {"rute": "Pejagan - Jakarta", "tarif": "169000"},
+  {"rute": "Brebes - Jakarta", "tarif": "190500"},
+  {"rute": "Pemalang - Jakarta", "tarif": "230000"},
+  {"rute": "Batang - Jakarta", "tarif": "269000"},
+  {"rute": "Weleri - Jakarta", "tarif": "308000"},
+  {"rute": "Kendal - Jakarta", "tarif": "319500"},
+];
+
+List infoTransJakarta = [
+  {"rute": "RUTE", "tarif": "TARIF"},
+  {"rute": "Merak - Jakarta", "tarif": "67500"},
+];
+
+List infoMrt = [
+  {"rute": "RUTE", "tarif": "TARIF"},
+  {"rute": "Merak - Jakarta", "tarif": "67500"},
+  {"rute": "Tangerang - Jakarta", "tarif": "23500"},
+  {"rute": "JORR - Cawang", "tarif": "16500"},
+];
+
+List infoLrt = [
+  {"rute": "RUTE", "tarif": "TARIF"},
+  {"rute": "Merak - Jakarta", "tarif": "67500"},
+  {"rute": "Tangerang - Jakarta", "tarif": "23500"},
+  {"rute": "JORR - Cawang", "tarif": "16500"},
+  {"rute": "Cikampek - Jakarta", "tarif": "20000"},
+  {"rute": "Subang - Jakarta", "tarif": "55000"},
+  {"rute": "Indramayu - Jakarta", "tarif": "81500"},
+  {"rute": "Kertajati - Jakarta", "tarif": "99000"},
+];
+
+List infoNull = [
+  {"rute": "Data tidak Ada", "tarif": "Data tidak ada"},
+];
